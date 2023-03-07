ea42b1980295e4c0f892a4d41a3c6c6c1cdc9321
s-sasaki-0529 <shingo.sasaki.0529@gmail.com>
2023-03-07 22:09:03 +0900

    Revert "コンポーネントの継承時に、継承した watch コールバックを上書きできるのか→できない"
    This reverts commit 7c8a9a351eb38434231106af8fdbf7ef1ad364ba.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-  <Child />
+  <h1>Hello, Vue 3</h1>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Parent from './components/Parent.vue'
-import Child from './components/Child.vue'
-export default defineComponent({
-  components: {
-    Parent,
-    Child
-  }
-})
+export default defineComponent({})
 </script>
